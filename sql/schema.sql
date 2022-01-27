ad82d7ddb2ec33e0695f31cacdfa1233994a9ad1
limich <limich@umich.edu>
2022-01-27 18:50:25 -0500

    12/27, updated schema and sql components

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,7 +9,54 @@ CREATE TABLE users(
   PRIMARY KEY(username)
 );
 CREATE TABLE posts(
-  postid int AUTO_INCREMENT NOT NULL,
+  postid int AUTO_INCREMENT,
   filename VARCHAR(64) NOT NULL,
-  PRIMARY KEY(postid)
-)
+  owner VARCHAR(20) NOT NULL,
+  created DATETIME,
+  PRIMARY KEY(postid),
+  FOREIGN KEY(owner) REFERENCES users(username)
+  CONSTRAINT posts_delete
+    FOREIGN KEY (owner)
+    REFERENCES users(username)
+    ON DELETE CASCADE
+);
+CREATE TABLE following(
+  username1 VARCHAR(20) NOT NULL,
+  username2 VARCHAR(20) NOT NULL,
+  created DATETIME,
+  PRIMARY KEY(username1, username2),
+  -- FOREIGN KEY (username1) REFERENCES users(username),
+  -- FOREIGN KEY (username2) REFERENCES users(username),
+  FOREIGN KEY(username1, username2) REFERENCES users(username, username) ON DELETE CASCADE
+  -- CONSTRAINT username2_delete FOREIGN KEY(username2) REFERENCES users(username) ON DELETE CASCADE
+);
+CREATE TABLE comments(
+  commentid int AUTO_INCREMENT,
+  owner VARCHAR(20) NOT NULL,
+  postid int NOT NULL,
+  text VARCHAR(1024) NOT NULL,
+  created DATETIME,
+  PRIMARY KEY(commentid),
+  -- FOREIGN KEY(owner) REFERENCES users(username),
+  -- FOREIGN KEY(postid) REFERENCES posts(postid),
+  FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY(postid) REFERENCES posts(postid) ON DELETE CASCADE
+);
+CREATE TABLE likes (
+  likeid int AUTO_INCREMENT,
+  owner VARCHAR(20) NOT NULL,
+  postid int NOT NULL,
+  created DATETIME,
+  PRIMARY KEY(likeid),
+  FOREIGN KEY (owner) REFERENCES users(username),
+  FOREIGN KEY(postid) REFERENCES posts(postid),
+  CONSTRAINT likes_owner_delete
+    FOREIGN KEY (owner)
+    REFERENCES users(username)
+    ON DELETE CASCADE,
+  CONSTRAINT likes_postid_delete
+    FOREIGN KEY (postid)
+    REFERENCES posts(postid)
+    ON DELETE CASCADE
+);
+
